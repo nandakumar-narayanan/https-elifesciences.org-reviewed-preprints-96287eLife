b0194786dea52338cc428b46508349f11beabe59
nandakumar-narayanan <nandakumar.narayanan@gmail.com>
2025-01-10 10:57:44 -0600

    Add files via upload

---
+++ AnimalList_mf.m
@@ -0,0 +1,71 @@
+Pharm
+PB1 and PB2: female
+PB3: male
+PB5 and PB6: female
+PB7 and PB8: male
+PB9: female
+PB11 and PB12: male
+
+D2
+IOL1: M
+IOL2:F
+IOR3: M
+SL10: M
+SL3: F
+SL6: M
+SL8: M
+SL9:F
+SR1: F
+SR2: F
+
+D1
+CL3:F  
+CL6:M
+CR2:M
+CR3:M
+CR5: F 
+CR6:F
+
+D2control
+icOL1: F
+icOL6: F
+icOR2: M
+icOR3: M
+icOR5: M
+
+
+D1control
+dcOL2: M
+dcOL3: M
+dcOR1: F
+dcOR5: F
+dcOR6: F
+
+
+Pharm/electrophysiology
+EP8_2019-11-07_11-42-42_PHARM-SALINE_roughsort_NN.nex F
+ER3_2020-04-10_14-59-00_SALINE_D1_roughsort-02_NN.nex M
+ER4_2020-04-16_16-23-44_SALINE_D2_roughsort_NN.nex M
+ER5_2020-04-13_18-47-12_SCH_D2_roughsort_NN.nex F
+dER1_2021-07-06_06-50-31_SALINE_d2_pt2_roughsort_NN.nex M
+dER6_2022-06-15_17-05-44_SALINE_roughsort-02_NN.nex M 
+iEL3_2021-11-02_15-39-32_SALINE_D1_roughsort_NN.nex F
+iEL5_2021-11-16_13-32-32_SCH_D1_roughsort_NN.nex M
+iER1_2020-12-14_16-19-40_SCH_D2_roughsort_NN.nex F
+iER2_2021-01-08_19-03-54_SCH_D1_roughsort_NN.nex M
+iER4_2021-11-09_16-34-37_SCH_D1_PT2_sorted_NN.nex M
+
+Tagging
+dEL7_2022-06-02_15-09-11_RD1_bin_roughsort_v2_NN.nex F
+dEL8_2022-06-29_17-18-02_RD7_bin_rousghsort_NN.nex F
+dER1_2021-07-12_07-24-09_LD1_bin_sort1_NN.nex M
+dER2_2021-08-24_11-01-15_LD4_bin_roughsort_NN-01.nex M
+dER9_2022-06-29_15-35-49_RD1_bin_roughsort_NN.nex M
+
+iEL3_2021-10-10_14-04-05_RD5_bin_sort2_NN.nex F
+iEL5_2021-10-11_16-29-10_RD3_bin_sort2_NN-01.nex M
+iER1_2020-12-18_16-57-23_LD2_kwik_sort2_NN.nex F
+iER4_2021-11-15_17-11-13_RD6_bin_sort2_NN.nex M
+
+
+
